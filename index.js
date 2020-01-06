4d6b03bb72f130ad7e0826b15977d588c4f38063
Richell Bonfield <richell.bonfield12@gmail.com>
2020-01-06 03:49:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,8 +16,9 @@ function wrapGifts(gifts) {
 wrapGifts(gifts);
 
 function writeCards(names, event) {
+   let thankYouCards = []
   for (let i = 0; i < names.length; i++) {
-    console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
   }
   
   return names;
